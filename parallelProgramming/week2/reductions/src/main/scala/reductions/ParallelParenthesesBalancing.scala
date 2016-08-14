ba353d8a8fdf0b5ec734a5b884eb9afff4a05d39
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2016-08-14 10:06:05 -0400

    Adding week2 assignment

---
+++ parallelProgramming/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -0,0 +1,88 @@
+package reductions
+
+import scala.annotation._
+import org.scalameter._
+import common._
+
+object ParallelParenthesesBalancingRunner {
+
+  @volatile var seqResult = false
+
+  @volatile var parResult = false
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 120,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val length = 100000000
+    val chars = new Array[Char](length)
+    val threshold = 10000
+    val seqtime = standardConfig measure {
+      seqResult = ParallelParenthesesBalancing.balance(chars)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential balancing time: $seqtime ms")
+
+    val fjtime = standardConfig measure {
+      parResult = ParallelParenthesesBalancing.parBalance(chars, threshold)
+    }
+    println(s"parallel result = $parResult")
+    println(s"parallel balancing time: $fjtime ms")
+    println(s"speedup: ${seqtime / fjtime}")
+  }
+}
+
+object ParallelParenthesesBalancing {
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def balance(chars: Array[Char]): Boolean = {
+    // Initialize counters
+    var acc = 0
+    var i = 0
+
+    // Loop through characters until we run out or find an imbalance
+    while (acc >= 0 && i < chars.length) {
+      if (chars(i) == '(') acc += 1
+      else if (chars(i) == ')') acc -= 1
+      i += 1
+    }
+
+    // Check if this is still 0
+    acc == 0
+  }
+
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
+
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+      if (idx == until) (arg1, arg2)
+      else chars(idx) match {
+        case ')' => traverse(idx + 1, until, arg1 - 1, Math.min(arg1 - 1, arg2))
+        case '(' => traverse(idx + 1, until, arg1 + 1, Math.min(arg1 + 1, arg2))
+        case _ => traverse(idx + 1, until, arg1, arg2)
+      }
+    }
+
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from <= threshold) traverse(from, until, 0, 0)
+      else {
+        val mid = (from + until) / 2
+        val ((l1, l2), (r1, r2)) = parallel(reduce(from, mid), reduce(mid, until))
+        (l1 + r1, Math.min(l2, l1 + r2))
+      }
+    }
+
+    reduce(0, chars.length) == (0, 0)
+  }
+
+  // For those who want more:
+  // Prove that your reduction operator is associative!
+
+}
